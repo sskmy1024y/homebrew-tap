61c34cd621ac599259af04b7fa8a6028ee1974d4
sho <sskmy1024.y@gmail.com>
2022-04-21 03:53:07 +0900

    update ovice app

--- Casks/ovice-app.rb
+++ Casks/ovice-app.rb
@@ -3,16 +3,15 @@ cask "ovice-app" do
   name "oVice app"
   desc "Desktop application for oVice user"
   homepage "https://github.com/sskmy1024y/oVice-app"
+  sha256 :no_check
 
   if Hardware::CPU.intel? 
-    url "https://github.com/sskmy1024y/oVice-app/releases/download/v#{version}/oVice-app_#{version}_x64.dmg"
-    sha256 "928d896ee874df0516e23e26bc6e0e5008d408e818cb4dbd0934c9ef0f705ca4"
+    url "https://github.com/sskmy1024y/oVice-app/releases/download/v#{version}/oVice-app-#{version}.dmg"
   else
-    url "https://github.com/sskmy1024y/oVice-app/releases/download/v#{version}/oVice-app_#{version}_arm64.dmg"
-    sha256 "c205e86434b9a83595c4e8758fbbe1e708e108084376d9a870f93a6362aca0eb"
+    url "https://github.com/sskmy1024y/oVice-app/releases/download/v#{version}/oVice-app-#{version}-arm64.dmg"
   end
 
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :sierra"
 
   app "oVice-app.app"
 
